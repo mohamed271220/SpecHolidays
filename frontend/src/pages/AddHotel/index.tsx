3b8f0746215c529abd9d03fc2d6db9776c9f9100
mohamed271220 <mohamedaprahym@gmail.com>
2024-07-25 17:40:43 +0300

    file upload

--- frontend/src/pages/AddHotel/index.tsx
+++ frontend/src/pages/AddHotel/index.tsx
@@ -1,13 +1,16 @@
 import { useMutation } from "react-query";
-import HotelForm, { HotelType } from "../../forms/HotelForm";
+import HotelForm from "../../forms/HotelForm";
 import { useAppContext } from "../../contexts/AppContext";
 import * as apiClient from "../../api-client";
+import { useNavigate } from "react-router-dom";
 
 const AddHotel = () => {
   const { showToast } = useAppContext();
+  const navigate = useNavigate()
   const { mutate, isLoading } = useMutation(apiClient.addHotel, {
     onSuccess: () => {
       showToast({ message: "Hotel added successfully", type: "SUCCESS" });
+      navigate("/");
     },
     onError: () => {
       showToast({ message: "Failed to add hotel", type: "ERROR" });
